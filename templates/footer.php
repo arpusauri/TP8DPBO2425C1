7105175a71d1d13a84e06a4f98f9d290b0538f09
arpusauri <aryapurnama877@gmail.com>
2025-11-17 20:48:30 +0700

    initial commit

---
+++ templates/footer.php
@@ -0,0 +1,11 @@
+</div>
+
+<!-- jQuery -->
+<script src="public/js/jquery.min.js"></script>
+
+<!-- Bootstrap + Popper -->
+<script src="public/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
---
+++ templates/header.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title><?= $title ?? 'Lecturers' ?></title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="public/css/bootstrap.min.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-dark bg-dark">
+        <a class="navbar-brand" href="index.php">Lecturers</a>
+    </nav>
+
+    <div class="container mt-4">
